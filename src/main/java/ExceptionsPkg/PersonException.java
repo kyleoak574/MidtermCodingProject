6cb74a5cba33c87f607bc80ffe156a19db841eb1
Kyle Oak <Kyle Oak@128.4.146.254>
2018-03-17 20:53:39 -0500

    Midterm

---
+++ src/main/java/ExceptionsPkg/PersonException.java
@@ -0,0 +1,10 @@
+package ExceptionsPkg;
+
+import com.cisc181.core.Person;
+
+public class PersonException extends Exception {
+	private Person person;
+	public PersonException(Person person) {
+		this.person=person;
+	}
+}
